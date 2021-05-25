7e7bde271442cf767fd3df9db10c2fe654e240e4
Emre Aka <aka548548@gmail.com>
2021-05-26 01:59:39 +0300

    Human Resources Management System. Swagger UI is added and more.

--- src/main/java/kodlama/io/hrms/dataAccess/abstracts/ActivisionEmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package kodlama.io.hrms.dataAccess.abstracts;
-
-import kodlama.io.hrms.entities.concretes.ActivisionEmployee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivisionEmployeeDao extends JpaRepository<ActivisionEmployee, Integer> {
-}
