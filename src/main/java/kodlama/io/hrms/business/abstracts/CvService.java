045585db6cdb57520f1a1a300df792ea3a1ac765
Emre Aka <aka548548@gmail.com>
2021-06-04 13:17:09 +0300

    Human Resources Management System.
    Cloudinary service is added.

--- src/main/java/kodlama/io/hrms/business/abstracts/CvService.java
+++ src/main/java/kodlama/io/hrms/business/abstracts/CvService.java
@@ -3,11 +3,14 @@ package kodlama.io.hrms.business.abstracts;
 import kodlama.io.hrms.core.utilities.results.DataResult;
 import kodlama.io.hrms.core.utilities.results.Result;
 import kodlama.io.hrms.entities.concretes.Cv;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 public interface CvService {
     Result add(Cv cv);
     DataResult<List<Cv>> getAll();
     DataResult<List<Cv>> findAllByEmployeeId(int employeeId);
+    public DataResult<String> uploadPhoto(int id, String file);
 }
--- src/main/java/kodlama/io/hrms/business/concretes/CvManager.java
+++ src/main/java/kodlama/io/hrms/business/concretes/CvManager.java
@@ -1,22 +1,23 @@
 package kodlama.io.hrms.business.concretes;
 
+import kodlama.io.hrms.adapters.concretes.CloudinaryServiceAdapter;
 import kodlama.io.hrms.business.abstracts.CvService;
-import kodlama.io.hrms.core.utilities.results.DataResult;
-import kodlama.io.hrms.core.utilities.results.Result;
-import kodlama.io.hrms.core.utilities.results.SuccessDataResult;
-import kodlama.io.hrms.core.utilities.results.SuccessResult;
+import kodlama.io.hrms.core.utilities.results.*;
 import kodlama.io.hrms.dataAccess.abstracts.CvDao;
 import kodlama.io.hrms.entities.concretes.Cv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 @Service
 public class CvManager implements CvService {
 
     @Autowired
     private CvDao cvDao;
-
+    @Autowired
+    private CloudinaryServiceAdapter cloudinaryServiceAdapter;
 
     @Override
     public Result add(Cv cv) {
@@ -32,5 +33,23 @@ public class CvManager implements CvService {
     @Override
     public DataResult<List<Cv>> findAllByEmployeeId(int employeeId) {
         return new SuccessDataResult<List<Cv>>(this.cvDao.findAllByEmployee_Id(employeeId), "Cvs listed successfully");
+    }
+
+    @Override
+    public DataResult<String> uploadPhoto(int id, String filePath) {
+        File file = new File(filePath);
+        Object object = this.cloudinaryServiceAdapter.upload(file).get("secure_url");
+        if ((object == null)) {
+            return new ErrorDataResult<String>(null, "Failed to load photo! Not found image.");
+
+        } else if (!this.cvDao.existsById(id)) {
+            return new ErrorDataResult<String>(null, "Failed to load photo! Not found cv.");
+        } else {
+            String secure_url = object.toString();
+            Cv cv = this.cvDao.findById(id).get();
+            cv.setPhoto(secure_url);
+            this.cvDao.save(cv);
+            return new SuccessDataResult<String>(secure_url, "Photo upload successfully.");
+        }
     }
 }
